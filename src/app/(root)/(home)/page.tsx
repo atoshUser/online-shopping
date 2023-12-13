117b255c233f05979d1e3b7e022bdf8fdd10f591
Abdurakhmon <abdurahmonavezdjanov1@gmail.com>
2023-12-13 11:56:46 +0500

    Initial E-commerce Project

---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Home = () => {
+
+    const api = process.env.NEXT_MAIN_API
+  return (
+    
+    <>
+    <div>Home</div>
+  
+    </>
+  )
+}
+
+export default Home
